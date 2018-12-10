a302c836d76667b9a7df65e12d7b1771f0b90246
Adrien Nortain <adrien.nortain@zenika.com>
2018-12-10 08:26:45 +0800

    feat(i18n): Add i18n mixin for components

---
+++ src/components/mixins/i18n.js
@@ -0,0 +1,63 @@
+import log from 'services/log'
+import i18n from 'services/i18n';
+
+/**
+ * Mixin binding a component to the i18n service.
+ * @description In order for the mixin to work, the class using it must not extend connectedCallback 
+ * or disconnectedCallback without calling the parent implementation.
+ * Once the mixin is applied, the class should provide its own {@link I18nComponent#localeChangedCallback} implementation
+ * to react on a locale change.
+ * @param {Class} clazz the class to bind
+ */
+export function I18nComponent(clazz) {
+    return class extends clazz {
+
+        /**
+         * Hook to the lifecycle of the component to subscribe to the i18n service.
+         * @override
+         */
+        connectedCallback() {
+            super.connectedCallback();
+            log.debug(`Subscribing ${this.componentName()} to i18n service`);
+            this._subscribeToI18nService();
+        }
+
+        /**
+         * Hook to the lifecycle of the component to unsubscribe to the i18n service.
+         * @override
+         */
+        disconnectedCallback() {
+            super.disconnectedCallback();
+            log.debug(`Unsubscribing ${this.componentName()} from i18n service`);
+            this._unsubscribeToI18nService();
+        }
+
+        /**
+         * React to a locale change.
+         * This is the function to override on the class using this mixin.
+         */
+        localeChangedCallback(locale) {
+            log.debug(`Locale change to ${locale} detected by ${this.componentName()}`);
+        }
+
+        /**
+         * Subscribe to the i18n service with {@link I18nComponent#localeChangedCallback} as a callback.
+         * @private
+         */
+        _subscribeToI18nService() {
+            this._i18nSubscription = i18n.subscribe((locale => {
+                this.localeChangedCallback.call(this, locale);
+            }).bind(this));
+        }
+
+        /**
+         * Unsubscribe to the i18n service.
+         * @private
+         */
+        _unsubscribeToI18nService() {
+            this._i18nSubscription.unsubscribe();
+        }
+    }
+}
+
+export default I18nComponent;
